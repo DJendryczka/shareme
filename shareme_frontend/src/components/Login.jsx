542eed51e34710eea284e180c3afe86754c6ba9e
Dariusz Jendryczka <88456245+DJendryczka@users.noreply.github.com>
2022-10-23 14:41:42 +0200

    Add Login component

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
-
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className='relative w-full h-full'>
+        <video
+         src={shareVideo}
+         type='video/mp4'
+         loop
+         muted
+         controls={false}
+         autoPlay
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
